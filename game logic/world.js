e17c85f5b52c0a82112f185417f2914133ca2477
SKLC1 <segevklein2000@gmail.com>
2022-04-28 17:00:27 +0300

    world

---
+++ game logic/world.js
@@ -0,0 +1,53 @@
+
+
+// grab HTML elements
+const grid = document.querySelector('.minecraft-container');
+
+
+// world matrix data
+const blocks ={
+  sky: 0,
+  dirt: 0
+}
+let matrix = [
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0,0],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1],
+]
+
+// world generation
+function generateWorld () {
+  for (let i = 0; i < matrix.length; i++) {
+      let row = document.createElement('div')
+      row.classList.add('world-row')
+      grid.append(row)
+    for (let j = 0; j < matrix[i].length; j++) {
+        if(matrix[i][j] === 0){
+          let block = document.createElement('div');
+          block.classList.add('block')
+          block.classList.add('sky')
+          row.append(block)
+        }
+        if(matrix[i][j] === 1){
+          let block = document.createElement('div');
+          block.classList.add('block')
+          block.classList.add('dirt')
+          row.append(block)
+        }
+    }
+  }
+}
+generateWorld()
+
+
+
+

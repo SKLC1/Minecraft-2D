0ac4d0d1f1cde36eff1ef1644bfb1e59ff43adaa
SKLC1 <segevklein2000@gmail.com>
2022-04-28 21:31:54 +0300

    pickaxe can mine stone bug

--- game logic/tools.js
+++ game logic/tools.js
@@ -2,20 +2,44 @@
 const pickaxe = document.querySelector('#pickaxe')
 const axe = document.querySelector('#axe')
 const shovel = document.querySelector('#shovel')
+let using = null;
 
 // tools implement
 const blockElements = document.querySelectorAll('.block');
 const blocksArr = [...blockElements];
+let minedBlocks = 0;
 
 
 const useShovel = () => {
-  if (shovel === document.activeElement) {
-    console.log('1');
+  using = 'shovel'
+  console.log(using);
+  for (let i = 0; i < blocksArr.length; i++) {
+    if (blocksArr[i].classList == 'block dirt') {
+      //  
+      if(using==='shovel'){
+      blocksArr[i].addEventListener('click',()=>{
+        blocksArr[i].classList.replace('dirt','sky')
+        minedBlocks ++;
+      })
+      }
+      // 
+    }
   }
+}
+shovel.addEventListener('click',useShovel)
+
+const usePickaxe = () => {
+  using = 'pickaxe'
+  console.log(using);
   for (let i = 0; i < blocksArr.length; i++) {
     blocksArr[i].addEventListener('click',()=>{
-      blocksArr[i].classList.replace('dirt','sky')
+      console.log(using);
+      if (using === 'pickaxe' && blocksArr[i].classList == 'block stone') {
+        // 
+        blocksArr[i].classList.replace('stone','sky')
+        minedBlocks ++;
+      }
     })
   }
 } 
-shovel.addEventListener('click',useShovel)
+pickaxe.addEventListener('click',usePickaxe)
--- game logic/world.js
+++ game logic/world.js
@@ -9,20 +9,42 @@ const blocks ={
   sky: 0,
   dirt: 0
 }
-let matrix = [
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1],
-]
+let matrix = []
+for (let i = 0; i < 5; i++) {
+  let row = []
+  matrix.push(row)
+  for (let j = 0; j < 5; j++) {
+    row.push(0)
+  }
+  matrix.push(row)
+  for (let j = 5; j < 8; j++) {
+    row.push(1)
+  }
+  matrix.push(row)
+  for (let j = 8; j < 11; j++) {
+    row.push(Math.floor(Math.random() * (3 - 1)) + 1)
+  }
+  matrix.push(row)
+  for (let j = 11; j < 15; j++) {
+    row.push(2)
+  }
+  
+}
+console.log(matrix);
+// let matrix = [
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [0,0,0,0,0,0,0,0,0,0,0,0],
+//   [1,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1],
+//   [1,1,1,1,1,1,1,1,1,1,1,1],
+//   [2,2,2,2,2,2,2,2,2,2,2,2],
+//   [2,2,2,2,2,2,2,2,2,2,2,2],
+// ]
 
 // world generation
 function generateWorld () {
@@ -40,6 +62,12 @@ function generateWorld () {
           let block = document.createElement('div');
           block.classList.add('block')
           block.classList.add('dirt')
+          row.append(block)
+        }
+        if(matrix[i][j] === 2){
+          let block = document.createElement('div');
+          block.classList.add('block')
+          block.classList.add('stone')
           row.append(block)
         }
     }
